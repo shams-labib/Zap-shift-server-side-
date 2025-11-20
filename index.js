451dcb9ced60ad11526b7657e525a8d96601abf3
shams al labib <samslabib19@gmail.com>
2025-11-21 00:08:46 +0600

    worked striped

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 const cors = require("cors");
 const port = process.env.PORT || 3000;
@@ -34,14 +35,65 @@ async function run() {
         query.senderEmail = email;
       }
 
-      const cursor = await parcelConnection.find(query).toArray();
+      const cursor = await parcelConnection
+        .find(query)
+        .sort({ createdAt: -1 })
+        .toArray();
       res.send(cursor);
+    });
+
+    app.get("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await parcelConnection.findOne(query);
+      res.send(result);
+    });
+
+    app.delete("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await parcelConnection.deleteOne(query);
+      res.send(result);
     });
 
     app.post("/parcels", async (req, res) => {
       const parcel = req.body;
+      parcel.createdAt = new Date();
       const result = await parcelConnection.insertOne(parcel);
       res.send(result);
+    });
+
+    // payment related Api's
+    app.post("/create-checkout-session", async (req, res) => {
+      const paymentInfo = req.body;
+
+      const amount = parseInt(paymentInfo.cost) * 100;
+
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, price_1234) of the product you want to sell
+            price_data: {
+              currency: "USD",
+              unit_amount: amount,
+              product_data: {
+                name: paymentInfo.parcelName,
+              },
+            },
+            quantity: 1,
+          },
+        ],
+        customer_email: paymentInfo.senderEmail,
+        mode: "payment",
+        metadata: {
+          parcelId: paymentInfo.parcelId,
+        },
+        success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success`,
+        cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
+      });
+
+      console.log(session);
+      res.send({ url: session.url });
     });
 
     await client.db("admin").command({ ping: 1 });
