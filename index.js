d54e2796ff0220fc2a9e86140289369222f8cbfa
shams al labib <samslabib19@gmail.com>
2025-11-21 02:52:19 +0600

    added stripe full method works

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const crypto = require("crypto");
+function genarateTrackingId() {
+  const prefix = "";
+  const date = new Date().toISOString().slice(0, 10).replace(/-g/);
+  const random = crypto.randomBytes(3).toString("hex").toUpperCase();
+  return `${prefix}-${date}-${random}`;
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gs1mqwb.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
@@ -26,6 +34,7 @@ async function run() {
 
     const db = client.db("zap_shift_db");
     const parcelConnection = db.collection("parcels");
+    const paymentsCollection = db.collection("payments");
 
     // parcels api
     app.get("/parcels", async (req, res) => {
@@ -64,6 +73,37 @@ async function run() {
     });
 
     // payment related Api's
+
+    app.post("/payment-checkout-session", async (req, res) => {
+      const paymentInfo = req.body;
+      const amount = parseInt(paymentInfo.cost) * 100;
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, price_1234) of the product you want to sell
+            price_data: {
+              currency: "usd",
+              unit_amount: amount,
+              product_data: {
+                name: `Please pay for ${paymentInfo.parcelName}`,
+              },
+            },
+            quantity: 1,
+          },
+        ],
+        mode: "payment",
+        metadata: {
+          parcelId: paymentInfo.parcelId,
+        },
+        customer_email: paymentInfo.senderEmail,
+        success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
+      });
+
+      res.send({ url: session.url });
+    });
+
+    // old
     app.post("/create-checkout-session", async (req, res) => {
       const paymentInfo = req.body;
 
@@ -87,6 +127,7 @@ async function run() {
         mode: "payment",
         metadata: {
           parcelId: paymentInfo.parcelId,
+          parcelName: paymentInfo.parcelName,
         },
         success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success`,
         cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
@@ -94,6 +135,54 @@ async function run() {
 
       console.log(session);
       res.send({ url: session.url });
+    });
+
+    // update
+    app.patch("/payment-success", async (req, res) => {
+      const sessionId = req.query.session_id;
+      const session = await stripe.checkout.sessions.retrieve(sessionId);
+      console.log("session retrieve", session);
+
+      const trackingId = genarateTrackingId();
+
+      if (session.payment_status === "paid") {
+        const id = session.metadata.parcelId;
+        const query = { _id: new ObjectId(id) };
+        const update = {
+          $set: {
+            paymentStatus: "paid",
+            trackingId: trackingId,
+          },
+        };
+        const result = await parcelConnection.updateOne(query, update);
+
+        const payment = {
+          amount: session.amount_total / 100,
+          currency: session.currency,
+          customerEmail: session.customer_email,
+          parcelId: session.metadata.parcelId,
+          parcelName: session.metadata.parcelName,
+          transactionId: session.payment_intent,
+          paymentStatus: session.payment_status,
+          paidAt: new Date(),
+          trackingId: "",
+        };
+
+        if (session.payment_status === "paid") {
+          const resultPayment = await paymentsCollection.insertOne(payment);
+          res.send({
+            success: true,
+            modifyParcel: result,
+            paymentInfo: resultPayment,
+            trackingId: trackingId,
+            transactionId: session.payment_intent,
+          });
+        }
+
+        // res.send(result);
+      }
+
+      res.send({ success: false });
     });
 
     await client.db("admin").command({ ping: 1 });
